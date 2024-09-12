4c45c3739475900b3da94886d239d2473de5593f
GUSPRIME <158516104+GUSDCPRIME@users.noreply.github.com>
2024-09-12 18:25:24 -0300

    Add files via upload

---
+++ atividade 7 python.py
@@ -0,0 +1,13 @@
+print("Programa para Cálculo de IMC")
+peso = float(input("Digite o seu peso (em kg): "))
+altura = float(input("Digite a sua altura (em metros): "))
+imc = peso / (altura**2)
+print (f"Seu IMC é (imc:.2f)")
+if(imc < 18.5):
+    print("Abaixo do peso")
+elif (18.5 <= imc < 25):
+    print ("Peso normal.")
+elif (25 <= imc <35):
+    print("Sobrepeso.")
+else:
+    print("Obesidade.")
