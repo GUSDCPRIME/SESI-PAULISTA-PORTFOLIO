bdb48bcc602b81ecda25a311b5061216d56ada0b
GUSPRIME <158516104+GUSDCPRIME@users.noreply.github.com>
2024-09-26 00:44:54 -0300

    Add files via upload

---
+++ python aula 1.py
@@ -0,0 +1,19 @@
+# Aqui eu posso adicionar comentários
+#continuar na linha de baixo
+print("Gustavo") # comentário a linha do código
+print('Gustavo')
+print('Gusatvo Henrique  "GUSPRIME" de Assunção Bezerra')
+print("Gusatvo Henrique  \"GUSPRIME\" de Assunção Bezerra")
+
+teste = "oi"
+print(teste)
+print(type(teste))
+teste = 17
+print(teste)
+print(type(teste))
+teste = 12.1
+print(teste)
+print(type(teste))
+teste = False
+print(teste)
+print(type(teste))
